b1aa53b5450d7c9c949d5047114960d44117c96a
Abdullahi Husain <abdulmohusain@gmail.com>
2024-04-28 00:35:12 +0100

    Corrected pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,10 @@
 """
 creating flask routes
 """
-from flask import jsonify
 from api.v1.views import app_views
+from flask import jsonify
 from models import storage
+
 
 @app_views.route('/status')
 def api_status():
@@ -13,6 +14,7 @@ def api_status():
     """
     response = {'status': 'OK'}
     return jsonify(response)
+
 
 @app_views.route('/stats')
 def get_stats():

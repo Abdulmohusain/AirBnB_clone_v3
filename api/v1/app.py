2e99d31fa4c63d08d8f3fc731cf229723d82585c
Abdullahi Husain <abdulmohusain@gmail.com>
2024-04-29 14:45:51 +0100

    404 Not found error

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 create new flask app
 """
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 app = Flask(__name__)
@@ -15,6 +15,15 @@ app.register_blueprint(app_views)
 def tear_down(status_code=None):
     """closes the db session by calling .close()"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    handler for 404 errors that returns a JSON-formatted
+    404 status code response
+    """
+    return jsonify({"error": "Not found"})
 
 
 if __name__ == "__main__":

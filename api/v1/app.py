8fec7ea4a2062c8647fcc74dbe911c67efeb000b
Abdullahi Husain <abdulmohusain@gmail.com>
2024-05-01 00:17:31 +0100

    Place

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,11 @@ create new flask app
 from os import getenv
 from flask import Flask, jsonify
 from models import storage
+from flask_cors import CORS
 from api.v1.views import app_views
-app = Flask(__name__)
 
+app = Flask(__name__)
+CORS(app, resource={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
